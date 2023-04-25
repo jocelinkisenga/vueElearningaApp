6a6809c58d80c7a0dfd7aa35247fadd4dd028a0b
jocelin kisenga <kisengajocelin7@gmail.com>
2023-04-26 00:06:30 +0200

    added router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import * as VueRouter from 'vue-router'
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    route : [
+        {
+            path: '/',
+            name : 'home',
+            component : 'home'
+        },
+        {
+            path: '/about',
+            name : 'about',
+            component : 'about'
+        },
+        {
+            path : '/contact',
+            name : 'contact',
+            component : 'contact'
+        }
+    ]
+});
+
+const app = createApp(App)
+app.use(router).mount('#app')
+
+app.mount('app')
