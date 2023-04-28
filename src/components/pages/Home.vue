89327bb1d216fdb21168e09fde449f29c8a09f8c
jocelin kisenga <kisengajocelin7@gmail.com>
2023-04-28 10:03:18 +0200

    added env file

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -5,7 +5,8 @@
 </template>
 <script>
 
-    const courses = fetch('http://127.0.0.1:8000/api/courses').then(response => response.json()).then(data => {
-        console.log(data);
-    })
+    // const courses = fetch('http://127.0.0.1:8000/api/courses').then(response => response.json()).then(data => {
+    //     console.log(data);
+    // })
+    
 </script>
