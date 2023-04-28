ca49e7ac20d4d6195b5b617bc3f7ccdc6dc57e58
jocelin kisenga <kisengajocelin7@gmail.com>
2023-04-28 11:05:34 +0200

    added home sanding

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,12 +1,34 @@
 <template>
    
     bonjour jocelin
-   
+   <div class="courses" v-for="course in courses" :key="course.id">
+    {{ course.title }}
+   </div>
 </template>
 <script>
+import {ref} from 'vue'
+import env from '@/env.js'
 
-    // const courses = fetch('http://127.0.0.1:8000/api/courses').then(response => response.json()).then(data => {
-    //     console.log(data);
-    // })
-    
+export default {
+    setup (){
+        // fetching courses from the data base
+        const courses = ref([])
+        const DbCourses  = fetch(`${env.elearning_url}courses`).then(
+            response => response.json()).then(
+                data => {
+                courses.value = data.data
+               
+                }
+            )
+        return {
+            courses
+        }
+        
+        
+        
+    }
+}
+
+
+
 </script>
